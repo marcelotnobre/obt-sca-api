7aa78362dc22eaf9650a97763116e2a7de265a26
Luiz Eduardo <luizedutl@gmail.com>
2019-12-03 18:03:04 -0200

    Refatoração do endpoint utilizado na recuperação de senha

--- src/main/java/br/com/obt/sca/api/service/UsuarioService.java
+++ src/main/java/br/com/obt/sca/api/service/UsuarioService.java
@@ -143,7 +143,7 @@ public class UsuarioService {
 
         Map<String, Object> variaveis = new HashMap<>();
         variaveis.put("emails", emails);
-        variaveis.put("url", property.getUrlApiScaClient() + "/recuperar_senha/token?" + token);
+        variaveis.put("url", property.getUrlApiScaClient() + "/recuperar_senha.jsf?token=" + token);
 
         logger.info("antes de enviar o email.");
         mailer.enviarEmailConfirmacaoDePermissaoCadastrada(emails,
