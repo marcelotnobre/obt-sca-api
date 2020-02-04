b982cbbee417d51d0ce76f9c2ff0c89130c8715c
fabricaOuterbox <48888102+fabricaOuterbox@users.noreply.github.com>
2020-02-03 22:04:46 -0300

    Update V02__registrar_tabelas_iniciais.sql

--- src/main/resources/db/migration/mysql/V02__registrar_tabelas_iniciais.sql
+++ src/main/resources/db/migration/mysql/V02__registrar_tabelas_iniciais.sql
@@ -122,8 +122,19 @@ values
 
 -- Estatistica
     (66, 'ROLE_VISUALIZAR_ESTATISTICA', 'Permissão para visualizar a estatistica', 2,true);
-
-
+    
+-- menu
+INSERT INTO permissao
+    (id, nome, descricao, sistema_id, status)
+values
+    (67, 'ROLE_VISUALIZAR_MENU_BIOMETRIA', 'Permissão para o menu de biometria', 1,true),
+    (68, 'ROLE_VISUALIZAR_MENU_AUTENTICACAO_BIOMETRIA', 'Permissão para visualizar o menu de autenticação por biometria', 1,true),
+    (69, 'ROLE_VISUALIZAR_MENU_RELATORIO', 'Permissão para visualizar o menu de relatório', 1,true),
+    (70, 'ROLE_VISUALIZAR_MENU_RELATORIO_USUARIO', 'Permissão para visualizar o relatório de usuário', 1,true),
+    (71, 'ROLE_VISUALIZAR_MENU_RELATORIO_ESTATISTICA', 'Permissão para visualizar o relatório de estatística', 1,true),
+    (72, 'ROLE_VISUALIZAR_MENU_LOG', 'Permissão para visualizar o menu de log', 1,true),
+    (73, 'ROLE_VISUALIZAR_MENU_LOG_REGISTRO_USUARIO_LOGIN', 'Permissão para visualizar o menu de log do usuário', 1,true),
+    (74, 'ROLE_VISUALIZAR_DASHBOARD_ESTATISTICA', 'Permissão para visualizar a dashboard de estatística', 1,true);
 
 -- Perfil
 INSERT INTO perfil 
@@ -205,7 +216,15 @@ values
     (1, 63),
     (1, 64),
     (1, 65),
-    (1, 66);
+    (1, 66),
+    (1, 67),
+    (1, 68),
+    (1, 69),
+    (1, 70),
+    (1, 71),
+    (1, 72),
+    (1, 73),
+    (1, 74);
 
 -- Perfil Usuario - Gestor
 INSERT INTO perfil_permissao
